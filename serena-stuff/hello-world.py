6c5c026e0cac9697cd1c5707dc01d18ce31bcb26
Serena Mower <serena.mower23@gmail.com>
2021-07-09 13:18:35 -0400

    added pandas to hello-world, created grades.csv and tested some pandas funcs

--- serena-stuff/hello-world.py
+++ serena-stuff/hello-world.py
@@ -2,3 +2,12 @@ import pandas as pd
 
 print('Hello World')
 
+grades = pd.read_csv('grades.csv')
+
+grade_pivot = grades.pivot(
+    columns="test",
+    index="student",
+    values="grade"
+)
+
+print(grade_pivot)
