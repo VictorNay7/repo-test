7fd47ff0770624d350d79efd69559f25d8910486
Moutwei <moutweileechap@gmail.com>
2021-07-09 19:02:50 -0400

    This file pulls data from a public API and displays the information.
    - my next goal is to add scanner functionality/ read user input.

--- moutwei/random_data.py
+++ moutwei/random_data.py
@@ -1,2 +1,23 @@
-print("test")
-print("woohoo")
+import pandas as pd
+import requests
+
+# Current: pulls in data from a weather api and dsiplays it
+# Goal: add scanner functionality 
+
+# URL to json data
+apiUrl = ("https://goweather.herokuapp.com/weather/Augusta")
+
+# GET data
+response = requests.get(apiUrl).json()
+
+# target temp
+# convert temp from celcius to farenheit
+temperature = response["temperature"]
+temp_farenheit = ((int(temperature[0]+temperature[1])) * 9/5) + 32
+
+# create dataframe from data
+# convert dataframe to csv for future use.
+df = pd.DataFrame(response.items())
+df = df.to_csv()
+
+print("It is " + str(temp_farenheit) + " degrees farenheit in Augusta, Maine.")
